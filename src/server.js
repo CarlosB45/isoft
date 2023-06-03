177df002fe7a360a566c6cabf9c2da130f36c625
Ivan Abut Blanco <68843754+idab2903@users.noreply.github.com>
2023-06-03 01:08:54 -0500

    inicio

--- src/server.js
+++ src/server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 3000;
-
-//
-
+const port = process.env.PORT || 5000;
 
 // Configuración de middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.get('/', (req, res) =>{
+  res.end('Bienvenido a HONDA')
+} )
 
 // Inicia el servidor
 app.listen(port, () => {
